1ec427c235a208f18d8e89cd51d0a4285ef06d25
Nadia Mounzih <nmounzih@yahoo.com>
2017-04-10 20:29:19 -0400

    Fixed population decimal places

--- djangomigrations/countries/models.py
+++ djangomigrations/countries/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Countries(models.Model):
     name = models.CharField(max_length=200)
     capital = models.CharField(max_length=200)
-    population = models.DecimalField(max_digits=3, decimal_places=3)
+    population = models.DecimalField(max_digits=10, decimal_places=5)
     aggregate = models.IntegerField(default=0)
     status = models.CharField(max_length=200)
     language = models.CharField(max_length=200)
